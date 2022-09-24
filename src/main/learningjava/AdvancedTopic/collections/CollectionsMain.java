0f4fa03f7de523c95848ea7a925bbd678f201642
Oluwafemi Ogundipe <52871023+Oluwafemijohn@users.noreply.github.com>
2022-09-24 03:17:26 +0100

    Done with the iterator interface

--- src/main/learningjava/AdvancedTopic/collections/CollectionsMain.java
+++ src/main/learningjava/AdvancedTopic/collections/CollectionsMain.java
@@ -1,6 +1,8 @@
 package main.learningjava.AdvancedTopic.collections;
 
 
+import main.learningjava.AdvancedTopic.generics.GenericList;
+
 import javax.management.openmbean.ArrayType;
 import java.util.List;
 
@@ -10,6 +12,17 @@ public class CollectionsMain {
      * Collections that are classes are ArrayList, LinkedList, HashSet, LinkedHashSet, TreeSet, PriorityQueue
      * */
     public static void main(String[] args) {
+        var list = new GenericList<String>();
+        list.add("a");
+        list.add("b");
+        var iterator = list.iterator();
+        while (iterator.hasNext()) {
+            var current = iterator.next();
+            System.out.println(current);
+        }
 
+        for(var item : list) {
+            System.out.println(item);
+        }
     }
 }
